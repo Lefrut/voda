ea5f745bebc2dd6e41799bb92cdbc5a801da519d
Kir D <zorozan21@gmai.com>
2025-10-06 16:48:59 +0300

    fix OrderMenuDTO mapper

--- app/src/main/java/com/m/vodovoz/data/vodovoz_service/model/OrderMenuDTO.kt
+++ app/src/main/java/com/m/vodovoz/data/vodovoz_service/model/OrderMenuDTO.kt
@@ -7,7 +7,7 @@ import androidx.annotation.Keep
 @Keep
 data class OrderMenuDTO(
     @Json(name = "MENU")
-    val MENU: List<MENU_DTO?>?,
+    val MENU: List<MENU_DTO>?,
     @Json(name = "ZAKAZ")
     val ZAKAZ: ZAKAZ_DTO?
 )
