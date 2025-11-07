47aeaf2481be99a749700e60abd28a0533b21132
Kir D <zorozan21@gmai.com>
2025-11-07 17:50:10 +0300

    add parallel hierarchy

--- app/src/main/java/com/m/vodovoz/ui/paging/PagingMviViewModel.kt
+++ app/src/main/java/com/m/vodovoz/ui/paging/PagingMviViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.CombinedLoadStates
 import androidx.paging.PagingData
 import com.m.vodovoz.ui.mvi.MviViewModel
-import com.m.vodovoz.util.extensions.debugLog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
@@ -18,54 +17,43 @@ abstract class PagingMviViewModel<ITEM : Any, S : PagingState<ITEM, S>, E>(
     state: S,
 ) : ItemsMviViewModel<ITEM, S, E>(state) {
 
-    private val pagingDataPresenterWrapper = PagingDataPresenterWrapper { snapshotList ->
-        updateState { it.withItems(snapshotList.mapNotNull { item -> item }) }
-    }
-
-    init {
-        listenLoadStates()
-    }
-
-    private fun listenLoadStates() = viewModelScope.launch {
-        pagingDataPresenterWrapper.collectLoadState { combinedLoadStates ->
-            updateState { s ->
-                s.withLoadStates(
-                    combinedLoadStates.copy(refresh = combinedLoadStates.refresh)
-                )
-            }
+    private val pagingDataViewModelManager = PagingDataViewModelManager(
+        scope = viewModelScope,
+        onListUpdate = { items ->
+            updateState { s -> s.withItems(items) }
+        },
+        onLoadStateUpdate = { loadStates ->
+            updateState { s -> s.withLoadStates(loadStates) }
         }
-    }
+    )
 
-    fun notifyPaging(itemIndex: Int) = viewModelScope.launch {
-        kotlin.runCatching { pagingDataPresenterWrapper[itemIndex] }
-    }
+    fun notifyPaging(itemIndex: Int) = pagingDataViewModelManager.notifyPaging(itemIndex)
+    protected suspend fun Flow<PagingData<ITEM>>.collectPagingData() =
+        pagingDataViewModelManager.collectPagingData(this)
 
-    protected suspend fun Flow<PagingData<ITEM>>.collectPagingData() = collect { pagingData ->
-        pagingDataPresenterWrapper.collectPagingData(pagingData)
-    }
 }
 
-abstract class PagingMviViewModel2<ITEM : Any, ITEM2 : Any, S : PagingState<ITEM, S>, E>(
+abstract class PagingMviViewModel2<ITEM1 : Any, ITEM2 : Any, S : PagingState2<ITEM1, ITEM2, S>, E>(
     state: S,
-) : ItemsMviViewModel<ITEM, S, E>(state) {
+) : ItemsMviViewModel2<ITEM1, ITEM2, S, E>(state) {
 
-    private val pagingManager1 = PagingDataViewModelManager<ITEM>(
+    private val pagingManager1 = PagingDataViewModelManager<ITEM1>(
         scope = viewModelScope,
-        onNewItems = {
-
+        onListUpdate = { items1 ->
+            updateState { s -> s.withItems1(items1) }
         },
-        onNewLoadStates = {
-
+        onLoadStateUpdate = { loadStates ->
+            updateState { s -> s.withLoadStates1(loadStates) }
         }
     )
 
     private val pagingManager2 = PagingDataViewModelManager<ITEM2>(
         scope = viewModelScope,
-        onNewItems = {
-
+        onListUpdate = { items2 ->
+            updateState { s -> s.withItems2(items2) }
         },
-        onNewLoadStates = {
-
+        onLoadStateUpdate = { loadStates ->
+            updateState { s -> s.withLoadStates2(loadStates) }
         }
     )
 
@@ -73,30 +61,34 @@ abstract class PagingMviViewModel2<ITEM : Any, ITEM2 : Any, S : PagingState<ITEM
 
     fun notifyPaging2(itemIndex: Int) = pagingManager2.notifyPaging(itemIndex)
 
-    suspend fun Flow<PagingData<ITEM>>.collectPagingData1() = pagingManager1.collectPagingData(this)
-    suspend fun Flow<PagingData<ITEM2>>.collectPagingData2() = pagingManager2.collectPagingData(this)
+    suspend fun Flow<PagingData<ITEM1>>.collectPagingData1() =
+        pagingManager1.collectPagingData(this)
+
+    suspend fun Flow<PagingData<ITEM2>>.collectPagingData2() =
+        pagingManager2.collectPagingData(this)
 
 }
 
 private class PagingDataViewModelManager<T : Any>(
     private val scope: CoroutineScope,
-    private val onNewItems: (List<T>) -> Unit,
-    private val onNewLoadStates: (CombinedLoadStates) -> Unit,
+    private val onListUpdate: (List<T>) -> Unit,
+    private val onLoadStateUpdate: (CombinedLoadStates) -> Unit,
 ) {
+
+    private val pagingDataPresenterWrapper = PagingDataPresenterWrapper { snapshotList ->
+        onListUpdate(snapshotList.items)
+    }
+
+    private fun listenLoadStates() = scope.launch {
+        pagingDataPresenterWrapper.collectLoadState { combinedLoadStates ->
+            onLoadStateUpdate(combinedLoadStates.copy(refresh = combinedLoadStates.refresh))
+        }
+    }
 
     init {
         listenLoadStates()
     }
 
-    private val pagingDataPresenterWrapper = PagingDataPresenterWrapper<T> { snapshotList ->
-        onNewItems(snapshotList.items)
-    }
-
-    private fun listenLoadStates() = scope.launch {
-        pagingDataPresenterWrapper.collectLoadState { combinedLoadStates ->
-            onNewLoadStates(combinedLoadStates.copy(refresh = combinedLoadStates.refresh))
-        }
-    }
 
     fun notifyPaging(itemIndex: Int) = scope.launch {
         kotlin.runCatching { pagingDataPresenterWrapper[itemIndex] }
@@ -123,11 +115,46 @@ abstract class ItemsMviViewModel<ITEM : Any, S : ItemsState<ITEM, S>, E> protect
             data
         }.shareIn(viewModelScope, SharingStarted.Eagerly, 1)
             .collect { data ->
-                debugLog { "collectItemsWith, data - $data" }
                 val result = updateItems(stateSnapshot.items, data)
-                updateState { it.withItems(result) }
+                updateState { s -> s.withItems(result) }
             }
     }
+
+}
+
+abstract class ItemsMviViewModel2<ITEM : Any, ITEM2 : Any, S : ItemsState2<ITEM, ITEM2, S>, E> protected constructor(
+    state: S,
+) : MviViewModel<S, E>(state) {
+
+
+    @Suppress("unused")
+    open suspend fun <T> collectItems1With(
+        source: Flow<T>,
+        updateItems: suspend (List<ITEM>, T) -> List<ITEM>,
+    ) {
+        state.map { it.items1 }.distinctUntilChanged().combine(source) { _, data ->
+            data
+        }.shareIn(viewModelScope, SharingStarted.Eagerly, 1)
+            .collect { data ->
+                val result = updateItems(stateSnapshot.items1, data)
+                updateState { s -> s.withItems1(result) }
+            }
+    }
+
+    @Suppress("unused")
+    open suspend fun <T> collectItems2With(
+        source: Flow<T>,
+        updateItems: suspend (List<ITEM2>, T) -> List<ITEM2>,
+    ) {
+        state.map { it.items2 }.distinctUntilChanged().combine(source) { _, data ->
+            data
+        }.shareIn(viewModelScope, SharingStarted.Eagerly, 1)
+            .collect { data ->
+                val result = updateItems(stateSnapshot.items2, data)
+                updateState { s -> s.withItems2(result) }
+            }
+    }
+
 
 }
 
@@ -166,18 +193,23 @@ abstract class PagingState<ITEM : Any, STATE> : ItemsState<ITEM, STATE>() {
     ): STATE
 }
 
-abstract class PagingState2<ITEM : Any, STATE> : ItemsState<ITEM, STATE>() {
+abstract class PagingState2<ITEM1 : Any, ITEM2 : Any, STATE> : ItemsState2<ITEM1, ITEM2, STATE>() {
 
-    abstract val loadStates: CombinedLoadStates
+    abstract val loadStates1: CombinedLoadStates
+    abstract val loadStates2: CombinedLoadStates
 
-    override fun withItems(newItems: List<ITEM>): STATE = copyPagingState(items = newItems)
+    fun withLoadStates1(newStates: CombinedLoadStates): STATE =
+        copyPagingState(loadStates1 = newStates)
 
-    fun withLoadStates(newStates: CombinedLoadStates): STATE =
-        copyPagingState(loadStates = newStates)
+    fun withLoadStates2(newStates: CombinedLoadStates): STATE =
+        copyPagingState(loadStates2 = newStates)
+
 
     protected abstract fun copyPagingState(
-        items: List<ITEM> = this.items,
-        loadStates: CombinedLoadStates = this.loadStates,
+        items1: List<ITEM1> = this.items1,
+        items2: List<ITEM2> = this.items2,
+        loadStates1: CombinedLoadStates = this.loadStates1,
+        loadStates2: CombinedLoadStates = this.loadStates2,
     ): STATE
 }
 
