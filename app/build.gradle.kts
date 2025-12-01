c974021c5c5679ff6eb77c94e75ede9e309f54f5
Kir D <zorozan21@gmai.com>
2025-12-01 17:19:51 +0300

    update app version to 2.1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,9 +26,9 @@ android {
         applicationId = "com.m.vodovoz"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2180
+        versionCode = 2190
         //todo
-        versionName = "2.1.8"
+        versionName = "2.1.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
