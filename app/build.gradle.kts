dbb5010d851f5117f94876273982b827819ed018
Kirill <zorozan21@gmail.com>
2026-01-15 12:36:00 +0300

    update version to 2.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,9 +38,9 @@ android {
         applicationId = "com.m.vodovoz"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2240
+        versionCode = 2250
         //todo
-        versionName = "2.2.4"
+        versionName = "2.2.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
