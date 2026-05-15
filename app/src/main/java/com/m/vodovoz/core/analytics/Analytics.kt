ecde9543a8f14360f4f24bde0aed4979ef946524
Kirill <zorozan21@gmail.com>
2026-05-15 17:03:42 +0300

    upgrade analytics class structure

--- app/src/main/java/com/m/vodovoz/core/analytics/Analytics.kt
+++ app/src/main/java/com/m/vodovoz/core/analytics/Analytics.kt
@@ -1,25 +1,39 @@
 package com.m.vodovoz.core.analytics
 
-import androidx.annotation.Keep
 import com.m.vodovoz.BuildConfig
 import io.appmetrica.analytics.AppMetrica
+import io.appmetrica.analytics.ecommerce.ECommerceEvent
 import kotlinx.coroutines.flow.StateFlow
 import org.json.JSONObject
 
-data object Analytics {
+data object Analytics :
+    AnalyticsEvents by (if (!BuildConfig.DEBUG) AnalyticsImpl else FakeAnalytics)
 
-    private val lock = Any()
+
+interface AnalyticsEvents {
+
+    fun setUserInstance(userId: StateFlow<Long?>) = Unit
+
+    fun reportEvent(name: String, block: EventParamsBuilder.() -> Unit = {}) = Unit
+
+    fun reportError(text: String, throwable: Throwable? = null) = Unit
+
+    fun reportEcommerce(event: ECommerceEvent) = Unit
+}
+
+
+private data object AnalyticsImpl : AnalyticsEvents {
     private lateinit var userIdState: StateFlow<Long?>
-
-
-    fun setUserInstance(userId: StateFlow<Long?>) = synchronized(lock) {
+    override fun setUserInstance(userId: StateFlow<Long?>) = synchronized(this) {
         if (::userIdState.isInitialized) return
         userIdState = userId
     }
 
-    @Keep
-    fun reportEvent(name: String, block: EventParamsBuilder.() -> Unit = {}) = runCatching {
-        if (!BuildConfig.DEBUG) {
+    override fun reportEvent(
+        name: String,
+        block: EventParamsBuilder.() -> Unit
+    ) {
+        runCatching {
             val userId = userIdState.value ?: "0"
             val builder = EventParamsBuilder().apply(block)
             val params = builder.build().toMutableMap().apply {
@@ -29,15 +43,18 @@ data object Analytics {
         }
     }
 
-
-    @Keep
-    fun reportError(text: String, throwable: Throwable? = null) = runCatching {
-        if (!BuildConfig.DEBUG) {
-            AppMetrica.reportError(text, throwable)
-        }
+    override fun reportError(text: String, throwable: Throwable?) {
+        AppMetrica.reportError(text, throwable)
     }
 
+    override fun reportEcommerce(event: ECommerceEvent) {
+        AppMetrica.reportECommerce(event)
+    }
+
+
 }
+
+data object FakeAnalytics : AnalyticsEvents
 
 class EventParamsBuilder {
     private val map = mutableMapOf<String, Any?>()
@@ -49,3 +66,4 @@ class EventParamsBuilder {
     fun build(): Map<String, Any?> = map
 }
 
+private const val CURRENCY_RUB = "RUB"
